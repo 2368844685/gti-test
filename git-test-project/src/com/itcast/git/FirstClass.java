d1922963097d79665cf698d2dcad8e6c4e9d162f
zhangsan <zhangsan@163.com>
2020-11-26 17:10:16 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itcast/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itcast.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
